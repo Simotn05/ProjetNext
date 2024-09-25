44055c63381c3c9585b634a2f90553915b8e885c
Yaser Afathi <yaser.af03@gmail.com>
2024-09-25 15:00:25 +0100

    final version

--- contexts/StudentContext.tsx
+++ contexts/StudentContext.tsx
@@ -1,26 +1,26 @@
-// import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode } from 'react';
 
-// interface StudentContextProps {
-//   studentId: number | null;
-//   setStudentId: React.Dispatch<React.SetStateAction<number | null>>;
-// }
+interface StudentContextProps {
+  studentId: number | null;
+  setStudentId: React.Dispatch<React.SetStateAction<number | null>>;
+}
 
-// const StudentContext = createContext<StudentContextProps | undefined>(undefined);
+const StudentContext = createContext<StudentContextProps | undefined>(undefined);
 
-// export const StudentProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-//   const [studentId, setStudentId] = useState<number | null>(null);
+export const StudentProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [studentId, setStudentId] = useState<number | null>(null);
 
-//   return (
-//     <StudentContext.Provider value={{ studentId, setStudentId }}>
-//       {children}
-//     </StudentContext.Provider>
-//   );
-// };
+  return (
+    <StudentContext.Provider value={{ studentId, setStudentId }}>
+      {children}
+    </StudentContext.Provider>
+  );
+};
 
-// export const useStudent = (): StudentContextProps => {
-//   const context = useContext(StudentContext);
-//   if (context === undefined) {
-//     throw new Error('useStudent must be used within a StudentProvider');
-//   }
-//   return context;
-// };
+export const useStudent = (): StudentContextProps => {
+  const context = useContext(StudentContext);
+  if (context === undefined) {
+    throw new Error('useStudent must be used within a StudentProvider');
+  }
+  return context;
+};
